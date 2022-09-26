951d9c53d2e178548567544d7b4a56580596abcc
harun ünal <hamidullahharun.unal@st.uskudar.edu.tr>
2022-09-26 19:48:30 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,33 +19,8 @@ class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes = [
     Quote(author: 'oscar wilde', text: 'everyone else is already taken'),
     Quote(author: 'oscar wilde', text: 'everyone else is already taken'),
-    Quote(author: 'oscar wilde', text: 'everyone else is already taken')
+    Quote(author: 'oscar wilde', text: 'everyone else is already takennnn')
   ];
-
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                quote.text,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.grey[600],
-                ),
-              ),
-              SizedBox(height: 6.0),
-              Text(
-                quote.author,
-                style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
-              )
-            ]),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +32,16 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes
+            .map((quote) => QuoteCard(
+                quote: quote,
+                delete: () {
+                  setState(() {
+                    quotes.remove(quote);
+                  });
+                }))
+            .toList(),
+        // children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
         // children: quotes.map((quote) {
         //   return Text('${quote.text} - ${quote.author}');
         // }).toList(),
